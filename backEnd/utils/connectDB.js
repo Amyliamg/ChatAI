bb4d371bc07f1137d49a0ad317146904ffbdb053
Amyliamg <xl2901@nyu.edu>
2024-02-03 15:08:37 -0600

    Remove comment

--- backEnd/utils/connectDB.js
+++ backEnd/utils/connectDB.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const MongoDBUrl = process.env.DB_CONNECTION_STRING;
-// mongodb+srv://xl2901:5fd80m87RkVNoeNJ@mern-ai.quw4n02.mongodb.net/?retryWrites=true&w=majority
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect( MongoDBUrl
