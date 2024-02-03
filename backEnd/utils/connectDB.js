36ba5fbdc9b9fef9a381462ecf6b34c1dd515266
Amyliamg <xl2901@nyu.edu>
2024-02-03 15:07:08 -0600

    Hide Url

--- backEnd/utils/connectDB.js
+++ backEnd/utils/connectDB.js
@@ -1,9 +1,11 @@
+
+require('dotenv').config();
 const mongoose = require("mongoose");
+const MongoDBUrl = process.env.DB_CONNECTION_STRING;
 // mongodb+srv://xl2901:5fd80m87RkVNoeNJ@mern-ai.quw4n02.mongodb.net/?retryWrites=true&w=majority
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://xl2901:5fd80m87RkVNoeNJ@mern-ai.quw4n02.mongodb.net/mern-ai?retryWrites=true&w=majority"
+    const conn = await mongoose.connect( MongoDBUrl
     );
 
     console.log(`Mongodb connected`);
